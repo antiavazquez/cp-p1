f104bfe34db7e82e1e5d965c317af33665440595
antiavazquez <antia.vazquezm@udc.es>
2025-02-14 09:54:37 +0100

    yipieeeeeeee

--- swap.c
+++ swap.c
@@ -193,10 +193,11 @@ void start_threads(struct options opt)
     }
     free(mutex);
     free(print_args->print_check);
+    free(print_thread);
+    free(print_args);
     free(args);
     free(threads);
     free(buffer.data);
-
     pthread_exit(NULL);
 }
 
